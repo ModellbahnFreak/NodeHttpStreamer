0947e96e45a3fb2dcd1a5ff9eb2a0b1d1a7dc2b3
ModellbahnFreak <freak.mdellbahn@gmail.com>
2021-03-16 18:24:51 +0100

    Added tester script

---
+++ testImgSequence.ts
@@ -0,0 +1,18 @@
+import * as fs from "fs";
+
+let counter = 1;
+
+let timer = setInterval(() => {
+    fs.readFile(
+        `./img/img${counter.toString(10).padStart(2, "0")}.jpg`,
+        (err, data) => {
+            if (!err && data) {
+                console.error(`Image ${counter - 1}`);
+                process.stdout.write(data);
+            } else {
+                console.error(err);
+            }
+        }
+    );
+    counter = (counter % 25) + 1;
+}, 1000);
