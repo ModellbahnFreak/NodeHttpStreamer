dbf9e762b9315079756561a0f6d0e2ebdf4e33fd
Georg Reißner <freak.mdellbahn@gmail.com>
2026-06-08 00:12:23 +0200

    Create cam.sh

---
+++ cam.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+rpicam-vid --awb auto --codec mjpeg --width 1920 --height 1080 -t 0 --inline -o - --framerate 50 | node /home/pi/Software/NodeHttpStreamer/out/index.js
+#additional options: --awbgains 2.5,2.3
